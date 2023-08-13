63e9b87dc2f2e5fdfe3c9c028e89acd18b837bcc
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-08-13 14:16:34 +0530

    initial commit

---
+++ mcr11/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import AllRoutes from "./Routes/routes";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <AllRoutes />
+    </div>
+  );
+}
+
+export default App;
